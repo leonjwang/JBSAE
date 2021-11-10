98355467e058f592079c04850e73bb410a921a3e
Leon Wang <83103060+lwang393@users.noreply.github.com>
2021-11-10 12:13:45 -0800

    Set

--- src/jbsae/struct/Set.java
+++ src/jbsae/struct/Set.java
@@ -2,8 +2,46 @@ package jbsae.struct;
 
 import java.util.*;
 
-public class Set<T> implements Iterable<T>{
+import static jbsae.util.Mathf.*;
+import static jbsae.util.Structf.*;
 
+public class Set<T> implements Iterable<T>{
+  public T[] table;
+	public int size = 0;
+
+  public Set(){
+    table = (T[])new Object[64];
+  }
+
+  public Set(Object... values){
+        this();
+        for(Object t : values) add((T)t);
+    }
+  
+  public void add(T value){
+    T[] table = this.table;
+
+        int h = value.hashCode();
+        int[] checks = new int[] {hash1(h), hash2(h), hash3(h)};
+        for(int i = 0;i < 3;i ++) if(eql(table[checks[i]], value)) return;
+
+        for(int i = 0;i < 3;i ++){
+          if(table[checks[i]] == null){
+            table[checks[i]] = value;
+            size ++;
+            return;
+          }
+        }
+
+        resize(table.length << 1);
+        add(value);
+  }
+
+  public int hash(int h, int prime){
+        h *= prime;
+        return (h ^ h >>>  (31 - trailZeros(table.length))) & (table.length - 1);
+  }
+  
     @Override
     public Iterator<T> iterator(){
         return null;
