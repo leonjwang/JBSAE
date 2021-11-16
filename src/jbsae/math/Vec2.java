552f5f6038fa78834b7052c2359e4e9f6c715969
Leon Wang <83103060+lwang393@users.noreply.github.com>
2021-11-16 13:01:34 -0800

    Vec2

--- src/jbsae/math/Vec2.java
+++ src/jbsae/math/Vec2.java
@@ -1,5 +1,7 @@
 package jbsae.math;
 
+import static jbsae.util.Mathf.*;
+
 public class Vec2{
     public float x, y;
 
@@ -7,11 +9,63 @@ public class Vec2{
     }
 
     public Vec2(float x, float y){
-        this.x = x;
-        this.y = y;
+        set(x, y);
     }
 
+    public Vec2(Vec2 v){
+        set(v);
+    }
 
+    public Vec2 set(float x, float y){
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public Vec2 set(Vec2 v){
+        return set(v.x, v.y);
+    }
+
+    public Vec2 add(float x, float y){
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+
+    public Vec2 add(Vec2 v){
+        return add(v.x, v.y);
+    }
+
+    public Vec2 scl(float x, float y){
+        this.x *= x;
+        this.y *= y;
+        return this;
+    }
+
+    public Vec2 scl(Vec2 v){
+        return scl(v.x, v.y);
+    }
+
+    public Vec2 scl(float v){
+        return scl(v, v);
+    }
+
+    public Vec2 rot(float a){
+        //TODO: rot
+        return null;
+    }
+
+    public Vec2 nor(){
+        return scl(1 / len());
+    }
+
+    public float len(){
+        return rt2(x * x + y * y);
+    }
+
+    public Vec2 cpy(){
+        return new Vec2(this);
+    }
 
     @Override
     public String toString(){
