3adb7c8710a54d573e59fe933e7d4b14c06f86ff
leonwang2 <leonwang297@gmail.com>
2025-08-19 16:35:39 -0700

    Update PosInput.java

--- src/jbsae/input/PosInput.java
+++ src/jbsae/input/PosInput.java
@@ -4,11 +4,24 @@ package jbsae.input;
 import org.lwjgl.glfw.*;
 
 import static jbsae.JBSAE.*;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
 
-public class PosInput extends GLFWCursorPosCallback{
+public class PosInput extends GLFWCursorPosCallback {
     @Override
-    public void invoke(long id, double x, double y){
+    public void invoke(long window, double x, double y) {
         input.pmouse.set(input.mouse);
-        input.mouse.set((float)x, curHeight - (float)y).scl((float)width / curWidth, (float)height / curHeight);
+
+        // Get the current window size in logical units
+        int[] winWidth = new int[1];
+        int[] winHeight = new int[1];
+        glfwGetWindowSize(window, winWidth, winHeight);
+
+        // Scale cursor from window coordinates → framebuffer coordinates
+        double mouseX = x * ((double) curWidth / winWidth[0]);
+        double mouseY = (winHeight[0] - y) * ((double) curHeight / winHeight[0]); // flip Y
+
+        // Map to your game's logical width/height
+        input.mouse.set((float) mouseX, (float) mouseY)
+        .scl((float) width / curWidth, (float) height / curHeight);
     }
 }
